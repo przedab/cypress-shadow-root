f89f0bf3d7845f8b4df8878e9020ad15943d506d
Przemysław Dąbrowski <pdabrowski@smartadserver.com>
2023-03-31 14:59:16 +0200

    shadow_root_issue

---
+++ cypress/e2e/shadow-root.cy.js
@@ -0,0 +1,11 @@
+describe("shadow_root_user_agent", () => {
+  it("access_to_pseudo_elements", () => {
+    // visit google player site
+    cy.visit("https://googleads.github.io/googleads-ima-html5/vsi/");
+    // get a pseudo element inside a shadow-root (user-agent)
+    cy.get("#h5-player")
+      .shadow()
+      .find('div[psudo="-webkit-media-controls"]')
+      .should("exist");
+  });
+});
